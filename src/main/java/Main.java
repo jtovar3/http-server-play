0e55fd518085c22c73084b005d0033817be2f1f8
jose tovar <jtovar@smc3.com>
2024-03-21 09:59:28 -0400

    pass 3rd test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,13 @@
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) {
@@ -23,18 +26,24 @@ public class Main {
              clientSocket = serverSocket.accept(); // Wait for connection from client.
              System.out.println("accepted new connection");
 
-             var inputS = new BufferedInputStream(clientSocket.getInputStream());
-             byte[] buffer = new byte[1024];    //If you handle larger data use a bigger buffer size
-             int read;
-             while((read = inputS.read(buffer)) != -1) {
-                 System.out.println(read);
-                 // Your code to handle the data
+             try (BufferedReader inputStreamReader = new BufferedReader(
+                     new InputStreamReader(clientSocket.getInputStream()));
+                  OutputStream outputStream = clientSocket.getOutputStream();
+             ) {
+                 String[] arg = inputStreamReader.readLine().split(" ");
+                 System.out.println(Arrays.toString(arg));
+                 String httpResponse;
+                 if(arg[1].equals("/")) {
+                     httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
+                 } else {
+                     httpResponse = "HTTP/1.1 400 BAD\r\n\r\n";
+                 }
+
+                 outputStream.write(httpResponse.getBytes(StandardCharsets.UTF_8));
+                 outputStream.flush();
              }
-             String httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
-             OutputStream outputStream = clientSocket.getOutputStream();
-             outputStream.write(httpResponse.getBytes(StandardCharsets.UTF_8));
-             outputStream.flush();
-             outputStream.close();
+
+             //clientSocket.close();
          }
   } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
