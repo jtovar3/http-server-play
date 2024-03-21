ff82de4d810c157ac20d30b121a176f0925e5d41
jose tovar <jtovar@smc3.com>
2024-03-21 11:25:04 -0400

    test 6th

---
+++ src/main/java/ClientHandler.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class ClientHandler extends Thread{
+	Socket clientSocket;
+
+	public ClientHandler(Socket clientSocket) {
+		this.clientSocket = clientSocket;
+	}
+
+	@Override
+	public void run() {
+		try (BufferedReader inputStreamReader = new BufferedReader(
+				new InputStreamReader(clientSocket.getInputStream()));
+			 OutputStream outputStream = clientSocket.getOutputStream();
+		) {
+			String[] arg = inputStreamReader.readLine().split(" ");
+			System.out.println(Arrays.toString(arg));
+			StringBuilder httpResponse = new StringBuilder();
+			if(arg[1].equals("/")) {
+				httpResponse.append("HTTP/1.1 200 OK\r\n\r\n");
+			} else if(arg[1].startsWith("/echo/")) {
+				String echo = arg[1].substring(6);
+				httpResponse.append("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n")
+						.append("Content-Length: " + echo.length() + "\r\n\r\n")
+						.append(echo);
+
+			} else if(arg[1].equals("/user-agent")) {
+				String s;
+				while(!(s = inputStreamReader.readLine()).isEmpty()) {
+					arg = s.split(" ");
+					if(arg[0].equalsIgnoreCase("USER-AGENT:")) {
+						break;
+					}
+				}
+				String echo = arg[1];
+				httpResponse.append("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n")
+						.append("Content-Length: " + echo.length() + "\r\n\r\n")
+						.append(echo);
+
+			} else {
+				httpResponse.append("HTTP/1.1 404 BAD\r\n\r\n");
+			}
+
+			outputStream.write(httpResponse.toString().getBytes(StandardCharsets.UTF_8));
+			outputStream.flush();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,8 +14,6 @@ public class Main {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-
      ServerSocket serverSocket = null;
      Socket clientSocket = null;
 
@@ -24,45 +22,10 @@ public class Main {
          serverSocket.setReuseAddress(true);
          while (true) {
              clientSocket = serverSocket.accept(); // Wait for connection from client.
+             if(clientSocket.isConnected()) {
+                 new ClientHandler(clientSocket).start();
              System.out.println("accepted new connection");
-
-             try (BufferedReader inputStreamReader = new BufferedReader(
-                     new InputStreamReader(clientSocket.getInputStream()));
-                  OutputStream outputStream = clientSocket.getOutputStream();
-             ) {
-                 String[] arg = inputStreamReader.readLine().split(" ");
-                 System.out.println(Arrays.toString(arg));
-                 StringBuilder httpResponse = new StringBuilder();
-                 if(arg[1].equals("/")) {
-                     httpResponse.append("HTTP/1.1 200 OK\r\n\r\n");
-                 } else if(arg[1].startsWith("/echo/")) {
-                     String echo = arg[1].substring(6);
-                     httpResponse.append("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n")
-                             .append("Content-Length: " + echo.length() + "\r\n\r\n")
-                             .append(echo);
-
-                 } else if(arg[1].equals("/user-agent")) {
-                     String s;
-                     while(!(s = inputStreamReader.readLine()).isEmpty()) {
-                         arg = s.split(" ");
-                         if(arg[0].equalsIgnoreCase("USER-AGENT:")) {
-                             break;
-                         }
-                     }
-                     String echo = arg[1];
-                     httpResponse.append("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n")
-                             .append("Content-Length: " + echo.length() + "\r\n\r\n")
-                             .append(echo);
-
-                 } else {
-                     httpResponse.append("HTTP/1.1 404 BAD\r\n\r\n");
-                 }
-
-                 outputStream.write(httpResponse.toString().getBytes(StandardCharsets.UTF_8));
-                 outputStream.flush();
              }
-
-             clientSocket.close();
          }
   } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
